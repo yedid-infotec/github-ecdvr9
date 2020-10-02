c7e16bb1d38c45c1ea813df6dfcc7a8bc1dae7b8
yedid-infotec <yedid.riquelme@infotec.mx>
2020-10-02 14:11:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-ecdvr9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-ecdvr9)
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'demo131';
+  users: any[] = [];
+
+  constructor(
+    protected userService: UserService
+  ) {
+  }
+
+  ngOnInit() {
+    this.userService.getUsers()
+    .subscribe(
+      (data) => { // Success
+        this.users = data['results'];
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+}
